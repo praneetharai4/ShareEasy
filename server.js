bcab2f4cbe5a1d0bc791ee27c6ddf5d7ecbb3131
Praneetha D Rai <praneethadrai@Praneethas-MacBook-Air.local>
2022-06-20 22:39:33 +0530

    second commit

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
 
+const connectDB = require('./config/db');
+connectDB();
+
 app.listen(PORT, ()=> {
     console.log(`Listening on port ${PORT}`);
 } )
