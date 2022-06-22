5475ca0b84bb58cf6ba20844d453966af0ca7e1e
Praneetha D Rai <praneethadrai@Praneethas-MacBook-Air.local>
2022-06-22 20:50:53 +0530

    initial

--- config/db.js
+++ config/db.js
@@ -5,11 +5,13 @@ const mongoose = require('mongoose');
 function connectDB()
 {
     mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser:true,
-    useCreateIndex:true, useUnifiedTopology: true, useFindAndModify:true});
+     useUnifiedTopology: true});
     const connection = mongoose.connection;
 
     connection.once('open', ()=>{
         console.log('Database connected');
+    }).on('error',function(err){
+        console.log('Connection failed');
     })
       
     
